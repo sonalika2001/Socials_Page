825e09a36ebe5eaa0c68410708592998ef97fb10
Sonalika Sahoo <sonalika2405@gmail.com>
2020-08-06 01:34:37 +0530

    core files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'socials_screen.dart';
 
 void main() {
   runApp(SocialsApp());
@@ -9,7 +10,7 @@ class SocialsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SocialsApp(),
+      home: SocialsScreen(),
     );
   }
 }
