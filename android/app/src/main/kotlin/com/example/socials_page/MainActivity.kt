e96901fe7be5c6a087c7d741566ef97cb7d3ddcd
Sonalika Sahoo <sonalika2405@gmail.com>
2020-08-05 21:53:07 +0530

    core files

---
+++ android/app/src/main/kotlin/com/example/socials_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.socials_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/socials_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SocialsScreen extends StatefulWidget {
+  @override
+  _SocialsScreenState createState() => _SocialsScreenState();
+}
+
+class _SocialsScreenState extends State<SocialsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
