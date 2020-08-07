06ba429379243f9f0e3aa9710a9263d937fe4bcf
pranshul <pranshul20162002@gmail.com>
2020-08-07 22:10:14 +0530

    edited socials_screen.dart. Added the switching functionality for the containers

--- lib/socials_screen.dart
+++ lib/socials_screen.dart
@@ -5,9 +5,82 @@ class SocialsScreen extends StatefulWidget {
   _SocialsScreenState createState() => _SocialsScreenState();
 }
 
+enum Social { instagram, youtube, twitter }
+
 class _SocialsScreenState extends State<SocialsScreen> {
+  Social social = Social.twitter;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              top: (social == Social.instagram)
+                  ? 0
+                  : MediaQuery.of(context).size.height * 0.666667,
+              left: 0,
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      social = Social.instagram;
+                    });
+                  },
+                  child: Container(
+                    height: (social == Social.instagram)
+                        ? MediaQuery.of(context).size.height * 0.666667
+                        : MediaQuery.of(context).size.height * (1 - 0.666667),
+                    width: (social == Social.instagram)
+                        ? MediaQuery.of(context).size.width
+                        : MediaQuery.of(context).size.width / 2.0,
+                    color: Colors.pinkAccent,
+                  ))),
+          Positioned(
+              top: (social == Social.twitter)
+                  ? 0
+                  : MediaQuery.of(context).size.height * 0.666667,
+              left: (social == Social.youtube)
+                  ? MediaQuery.of(context).size.width / 2.0
+                  : 0,
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      social = Social.twitter;
+                    });
+                  },
+                  child: Container(
+                    height: (social == Social.twitter)
+                        ? MediaQuery.of(context).size.height * 0.666667
+                        : MediaQuery.of(context).size.height * (1 - 0.666667),
+                    width: (social == Social.twitter)
+                        ? MediaQuery.of(context).size.width
+                        : MediaQuery.of(context).size.width / 2.0,
+                    color: Colors.blue,
+                  ))),
+          Positioned(
+              top: (social == Social.youtube)
+                  ? 0
+                  : MediaQuery.of(context).size.height * 0.666667,
+              left: (social == Social.youtube)
+                  ? 0
+                  : MediaQuery.of(context).size.width / 2.0,
+              child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      social = Social.youtube;
+                    });
+                  },
+                  child: Container(
+                    height: (social == Social.youtube)
+                        ? MediaQuery.of(context).size.height * 0.666667
+                        : MediaQuery.of(context).size.height * (1 - 0.666667),
+                    width: (social == Social.youtube)
+                        ? MediaQuery.of(context).size.width
+                        : MediaQuery.of(context).size.width / 2.0,
+                    color: Colors.red,
+                  ))),
+        ],
+      ),
+    );
   }
 }
