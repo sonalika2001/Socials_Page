2a1cf788629d27031c76a2818b27c629ff2a9fd9
Sanya Gupta <sanyagupta2001@yahoo.com>
2020-08-12 01:50:01 +0530

    added blur

--- lib/container_properties.dart
+++ lib/container_properties.dart
@@ -1,28 +1,83 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ContainerProperties extends StatelessWidget {
   final Color colour;
   final IconData icon;
+  final bool enabled;
+  Widget child;
 
-  ContainerProperties({@required this.colour, this.icon});
+  ContainerProperties({
+    @required this.colour,
+    this.icon,
+    this.enabled = false,
+    @required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Stack(
         children: [
-          Icon(
-            icon,
-            size: 70,
-            color: colour,
-          )
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              child: child,
+            ),
+          ),
+          (!enabled)
+              ? Container(
+                  child: Blur(
+                    opacity: 0.8,
+                    blur: 0.6,
+                    shade: Colors.grey[100],
+                  ),
+                )
+              : SizedBox.shrink(),
+          (!enabled)
+              ? Align(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    icon,
+                    size: 70,
+                    color: colour,
+                  ),
+                )
+              : SizedBox.shrink(),
         ],
       ),
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
         color: Color(0xFF1B1C21),
         borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
+
+class Blur extends StatelessWidget {
+  final double opacity;
+  final double blur;
+  final Color shade;
+
+  Blur({
+    this.opacity,
+    this.blur,
+    this.shade,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+      child: Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            color: shade.withOpacity(opacity),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       ),
     );
   }
