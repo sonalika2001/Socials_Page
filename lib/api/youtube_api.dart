f3373ce810c3d5964115d36f805c43d3436d8f49
Sonalika Sahoo <sonalika2405@gmail.com>
2020-08-13 23:38:29 +0530

    modified due to changes in json format

--- lib/api/youtube_api.dart
+++ lib/api/youtube_api.dart
@@ -7,16 +7,16 @@ class GetYoutubeInfo {
       []; //will contain list of urls (on calling getYoutubeInfo.youtubePosts() ) that link to the youtube video
   static List thumbnailURL = []; //will contain list of urls of thumbnails
   static List videoTitle = []; //list of the video titles
+  static List videoDuration = []; //list of the video durations
 
   static Future getData() async {
     try {
       http.Response _response = await http.get(url);
-      if(_response.statusCode == 200){
+      if (_response.statusCode == 200) {
         var body = jsonDecode(_response.body);
         var video = body['data'];
         return video;
-      }
-      else
+      } else
         print(_response.statusCode);
     } catch (e) {
       print(e);
@@ -26,13 +26,18 @@ class GetYoutubeInfo {
   static Future youtubePosts() async {
     //this function needs to be called to fetch the data for videoURL,thumbnailURL,videoTitle and store it in the above mentioned lists
     var posts = await getData();
-if(posts != null) {
-  for (var post in posts.keys) {
-    //looping through data
-    videoTitle.add('$post'); //added caption
-    videoURL.add(posts[post]['link']); //added video url
-    thumbnailURL.add(posts[post]['thumbnail']); //added thumbnail url
-  }
-}
+    if (posts != null) {
+      for (var post in posts) {
+        //looping through data
+        videoTitle.add('${post['title']}'); //added caption
+        videoURL.add('${post['link']}'); //added video url
+        thumbnailURL.add('${post['thumbnail']}'); //added thumbnail url
+        videoDuration.add('${post['time']}');
+      }
+      print(videoDuration);
+      print(videoURL);
+      print(thumbnailURL);
+      print(videoTitle);
+    }
   }
 }
