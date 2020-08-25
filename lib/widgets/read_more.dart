d79457133709f59682a80008c6cbbc7ef19cb545
Sanya Gupta <sanyagupta2001@yahoo.com>
2020-08-26 00:07:37 +0530

    caption editing

---
+++ lib/widgets/read_more.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'instagram_widget.dart';
+
+class ReadMore extends StatefulWidget {
+  String captionText;
+
+  ReadMore(this.captionText);
+
+  @override
+  _ReadMoreState createState() => _ReadMoreState();
+}
+
+class _ReadMoreState extends State<ReadMore> {
+  bool flag = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(widget.captionText,
+              maxLines: flag ? 8 : 2, textAlign: TextAlign.start),
+          InkWell(
+            onTap: () {
+              setState(() {
+                flag = !flag;
+              });
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                flag
+                    ? Text(
+                        "show less",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    : Text("...more", style: TextStyle(color: Colors.grey))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
